1c5c7bdfc9205dd4a176d147fc9bf8e93d316112
olafkfreund <olaf.loken@gmail.com>
2025-06-11 11:19:19 +0100

    chore: bump version to 1.0.0
    - Update version in flake.nix from 0.1.0 to 1.0.0
    - Update default version in package.nix to 1.0.0
    - Prepare for 1.0.0 release

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
     in {
       packages.default = self.packages.${system}.nixai;
       packages.nixai = pkgs.callPackage ./package.nix {
-        version = "0.1.0";
+        version = "1.0.0";
         src = ./.; # Use src instead of srcOverride for direct source
         rev = self.rev or null;
         gitCommit =
