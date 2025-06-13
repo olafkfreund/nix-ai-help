e76d45b482dd753e6559645482f2fa11ed43c3e6
olafkfreund <olaf.loken@gmail.com>
2025-06-13 12:51:56 +0100

    feat: Bump version from 1.0.2 to 1.0.5 in package.nix

--- package.nix
+++ package.nix
@@ -4,7 +4,7 @@
   installShellFiles,
   fetchFromGitHub,
   # Optional parameters for version/commit overrides
-  version ? "1.0.2",
+  version ? "1.0.5",
   src ? null,
   srcOverride ? null,
   rev ? null,
